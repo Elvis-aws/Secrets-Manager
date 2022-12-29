87a62cceeac8a152801c1160225a38ac9c5090b2
Elvis Ngwesse <elvisngwesseaws@gmail.com>
2022-12-29 00:59:09 +0000

    Updates

---
+++ secret-manager.r
@@ -0,0 +1,9 @@
+
+
+Secret Manager
+    - For storing secrets 
+    - Automates and forces rotation
+    - Integration with RDS databases
+    - Automatic secret rotation using Lambda 
+    - KMS Encryption is mandatory 
+    
